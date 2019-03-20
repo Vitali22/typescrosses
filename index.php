cffcb0e6d0e474eb9a7f06669856b5d55d1cc947
Emilio Vitali Padilla Socconini <emilio.padilla@ceeo.com.mx>
2019-03-20 09:55:28 -0600

    Estructura de cromosomas creada falta llenar los datos del JSON

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div id="art">
+  <div class="fila">
+    <div class ="pixelNegro"></div>
+    <div class ="pixelBlanco"></div>
+    <div class ="pixelBlanco"></div>
+    <div class ="pixelBlanco"></div>
+    <div class ="pixelBlanco"></div>
+    <div class ="pixelBlanco"></div>
+    <div class ="pixelBlanco"></div>
+    <div class ="pixelNegro"></div>
+    <div class ="pixelBlanco"></div>
+    <div class ="pixelNegro"></div>
+    <div class ="pixelBlanco"></div>
+    <div class ="pixelNegro"></div>
+    <div class ="pixelBlanco"></div>
+    <div class ="pixelBlanco"></div>
+    <div class ="pixelNegro"></div>
+    <div class ="pixelBlanco"></div>
+    <div class ="pixelBlanco"></div>
+    <div class ="pixelNegro"></div>
+    <div class ="pixelBlanco"></div>
+    <div class ="pixelBlanco"></div>
+    <div class ="pixelBlanco"></div>
+  </div>
+</div>
+<br>
+
+</body>
+</html>
