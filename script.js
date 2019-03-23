c6172b915e588dc5192a124a5def1be7dc468458
Emilio Vitali Padilla Socconini <esocconini@gmail.com>
2019-03-23 02:45:00 -0600

    The fill population function was added

--- script.js
+++ script.js
@@ -1,64 +1,30 @@
 new Vue({
-  el: '#v-for-object',
-  data: {
-
-  gene: {
-		
-    	cromosoma1:"bluePixel",
-    	cromosoma2:"redPixel",
-    	cromosoma3:"bluePixel",
-    	cromosoma4:"redPixel",
-    	cromosoma5:"bluePixel",
-    	cromosoma6:"redPixel",
-    	cromosoma7:"bluePixel",
-    	cromosoma8:"redPixel",
-    	cromosoma9:"bluePixel",
-    	cromosoma10:"redPixel",
-    	cromosoma11:"bluePixel",
-    	cromosoma12:"redPixel",
-    	cromosoma13:"bluePixel",
-    	cromosoma14:"redPixel",
-    	cromosoma15:"bluePixel",
-    	cromosoma16:"redPixel",
-    	cromosoma17:"bluePixel",
-    	cromosoma18:"redPixel",
-    	cromosoma19:"bluePixel",
-    	cromosoma20:"redPixel"
-		},
-/*
-        gene: {
-    	cromosoma1:colorPixel(),
-    	cromosoma2:colorPixel(),
-    	cromosoma3:colorPixel(),
-    	cromosoma4:colorPixel(),
-    	cromosoma5:colorPixel(),
-    	cromosoma6:colorPixel(),
-    	cromosoma7:colorPixel(),
-    	cromosoma8:colorPixel(),
-    	cromosoma9:colorPixel(),
-    	cromosoma10:colorPixel(),
-    	cromosoma11:colorPixel(),
-    	cromosoma12:colorPixel(),
-    	cromosoma13:colorPixel(),
-    	cromosoma14:colorPixel(),
-    	cromosoma15:colorPixel(),
-    	cromosoma16:colorPixel(),
-    	cromosoma17:colorPixel(),
-    	cromosoma18:colorPixel(),
-    	cromosoma19:colorPixel(),
-    	cromosoma20:colorPixel()
-		},
-*/
+	el: '#v-for-object',
+	data: {
+		population: [],
+		countGene: 0,
+		countChromosome: 0,
 	},
 	methods: {
-		colorPixel : function(){
-			if((Math.random())>0){
-				return "bluePixel";
+		colorPixel: function () {
+			if ((Math.random()) >= 0.5) {
+				return value = "bluePixel";
 			}
-			else{
-				return "redPixel";
+			else {
+				return value = "redPixel";
 			}
-    }
+		},
+		fillPopulation: function () {
+			for (countGene = 0; countGene < 100; countGene++) {
+				gene = [];
+				for (countChromosome = 0; countChromosome < 20; countChromosome++) {
+					cromosoma = '';
+					cromosoma = this.colorPixel();
+					gene.push(cromosoma)
+				}
+				this.population.push(gene)
+			};
+		}
 	},
 	mounted() {
 		//this.colorPixel;
