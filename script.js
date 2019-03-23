b42749d1352c35cc477d8acb56009c19cd05e680
Emilio Vitali Padilla Socconini <esocconini@gmail.com>
2019-03-22 19:40:03 -0600

    Changed the color pixels and started to randomize

--- script.js
+++ script.js
@@ -1,35 +1,65 @@
 new Vue({
   el: '#v-for-object',
   data: {
-    object: {
-      firstName: 'John',
-      lastName: 'Doe',
-      age: 30
-    },
 
+  gene: {
+    	cromosoma1:"bluePixel",
+    	cromosoma2:"redPixel",
+    	cromosoma3:"bluePixel",
+    	cromosoma4:"redPixel",
+    	cromosoma5:"bluePixel",
+    	cromosoma6:"redPixel",
+    	cromosoma7:"bluePixel",
+    	cromosoma8:"redPixel",
+    	cromosoma9:"bluePixel",
+    	cromosoma10:"redPixel",
+    	cromosoma11:"bluePixel",
+    	cromosoma12:"redPixel",
+    	cromosoma13:"bluePixel",
+    	cromosoma14:"redPixel",
+    	cromosoma15:"bluePixel",
+    	cromosoma16:"redPixel",
+    	cromosoma17:"bluePixel",
+    	cromosoma18:"redPixel",
+    	cromosoma19:"bluePixel",
+    	cromosoma20:"redPixel"
+		},
+/*
         gene: {
-    	cromosoma1:'pixelNegro',
-    	cromosoma2:'pixelBlanco',
-    	cromosoma3:'pixelNegro',
-    	cromosoma4:'pixelBlanco',
-    	cromosoma5:'pixelNegro',
-    	cromosoma6:'pixelBlanco',
-    	cromosoma7:'pixelNegro',
-    	cromosoma8:'pixelBlanco',
-    	cromosoma9:'pixelNegro',
-    	cromosoma10:'pixelBlanco',
-    	cromosoma11:'pixelNegro',
-    	cromosoma12:'pixelBlanco',
-    	cromosoma13:'pixelNegro',
-    	cromosoma14:'pixelBlanco',
-    	cromosoma15:'pixelNegro',
-    	cromosoma16:'pixelBlanco',
-    	cromosoma17:'pixelNegro',
-    	cromosoma18:'pixelBlanco',
-    	cromosoma19:'pixelNegro',
-    	cromosoma20:'pixelBlanco'
+    	cromosoma1:colorPixel(),
+    	cromosoma2:colorPixel(),
+    	cromosoma3:colorPixel(),
+    	cromosoma4:colorPixel(),
+    	cromosoma5:colorPixel(),
+    	cromosoma6:colorPixel(),
+    	cromosoma7:colorPixel(),
+    	cromosoma8:colorPixel(),
+    	cromosoma9:colorPixel(),
+    	cromosoma10:colorPixel(),
+    	cromosoma11:colorPixel(),
+    	cromosoma12:colorPixel(),
+    	cromosoma13:colorPixel(),
+    	cromosoma14:colorPixel(),
+    	cromosoma15:colorPixel(),
+    	cromosoma16:colorPixel(),
+    	cromosoma17:colorPixel(),
+    	cromosoma18:colorPixel(),
+    	cromosoma19:colorPixel(),
+    	cromosoma20:colorPixel()
+		},
+*/
+	},
+	methods: {
+		colorPixel : function(){
+			if((Math.floor(Math.random() * (10 - 1 + 1)) + 1)>0){
+				return "bluePixel";
+			}
+			else{
+				return "redPixel";
+			}
     }
-
-
-  }
+	},
+	mounted() {
+		//this.colorPixel;
+	},
 })
